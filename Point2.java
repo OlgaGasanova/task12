9dd2e8789544ffa0829646717208f088ccb9b52b
OlgaGasanova <Olga_Gasanova@epam.com>
2016-09-27 14:23:04 +0300

    add Point2

---
+++ Point2.java
@@ -0,0 +1,21 @@
+package lineAndPoints;
+
+public class Point {
+
+	private float x;
+	private float y;
+	
+	public Point(float x, float y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public float getY() {
+		return this.y;
+	}
+
+	public float getX() {
+		return this.x;
+	}
+
+}
